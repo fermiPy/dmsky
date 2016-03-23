5845a49f8eeedf7b28d23cc88c1c4345b14269b6
Alex Drlica-Wagner <kadrlica@fnal.gov>
2016-03-22 23:59:19 -0500

    Added versioneer

--- dmsky/__init__.py
+++ dmsky/__init__.py
@@ -4,11 +4,9 @@ Dark matter skymaps.
 __author__ = "Alex Drlica-Wagner"
 __email__ = "kadrlica@fnal.gov"
 
-
 # Automatically grab the version from the git tag to ensure that the
-# tag and the code agree. For more details, see `get_version.py`
-try:
-    from .version import __version__
-except ImportError:
-    from .get_version import get_version
-    __version__ = get_version()
+# tag and the code agree. For more details, see `python-versioneer`
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
