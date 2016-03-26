f0af5af64cd152a33832f9f69d764d9b3f5e44c7
Alex Drlica-Wagner <kadrlica@fnal.gov>
2016-03-26 16:35:20 -0500

    Update to pymodeler; added density tests

---
+++ tests/test_density.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+"""
+Generic python script.
+"""
+__author__ = "Alex Drlica-Wagner"
+import sys
+from collections import OrderedDict as odict
+import inspect
+
+import numpy as np
+import pylab as plt
+
+import dmsky.density
+
+dp = dmsky.density.DensityProfile()
+
+module = 'dmsky.density'
+fn = lambda member: inspect.isclass(member) and member.__module__==module
+classes = odict(inspect.getmembers(sys.modules[module], fn))
+
+radius = np.linspace(0.001,5,1000)
+
+# Plot the different profiles
+def test_densities():
+    fig,ax = plt.subplots()
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    for i,cls in enumerate(classes):
+        if cls == 'DensityProfile': continue
+        dp = dmsky.density.factory(cls)
+        dp.set_rho_r(1,dp.rs)
+        print dp
+        # Small offset for visualization
+        epsilon = i*0.2e-1
+        #epsilon = 0
+        plt.plot(radius,dp(radius) * (1+epsilon),'-',label=cls)
+        print
+    plt.xlabel("Radius")
+    plt.ylabel("Density")
+    plt.legend()
+
+test_densities()
+
+def test_min_max():
+    fig,ax = plt.subplots()
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+     
+    dp = dmsky.density.factory('NFWProfile',rmin=0.1)
+    print dp
+    epsilon = 0*2e-2
+    plt.plot(radius,dp(radius) * (1+epsilon),'-',label='rmin=%g'%dp.rmin)
+     
+    dp = dmsky.density.factory('NFWProfile',rmax=2)
+    print dp
+    epsilon = 1*2e-2
+    plt.plot(radius,dp(radius) * (1+epsilon),'-',label='rmax=%g'%dp.rmax)
+     
+    dp = dmsky.density.factory('NFWProfile',rhomax=100)
+    print dp
+    epsilon = 2*2e-2
+    plt.plot(radius,dp(radius) * (1+epsilon),'-',label='rhomax=%g'%dp.rhomax)
+     
+    plt.xlabel("Radius")
+    plt.ylabel("Density")
+    plt.legend()
+     
+     
+    plt.ion()
+    plt.show()
+
+test_min_max()
